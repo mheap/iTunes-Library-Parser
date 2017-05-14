2a31b0d25cf248d1a119f8470e845359520478d0
Michael Nordmeyer <michaelnordmeyer@users.noreply.github.com>
2017-05-14 10:44:38 +0200

    Housekeeping: Explicit code
    - Moved star rating to class iTunesLibrary
    - Renamed single-letter variables for better understanding
    - Converted tabs to spaces to play nicely with Github source views

--- iTunesLibrary.php
+++ iTunesLibrary.php
@@ -1,27 +1,50 @@
 <?php
 
 class track {}
-class iTunesLibrary{ 
-	private $_tracks; 
+class iTunesLibrary { 
+  private $_tracks; 
 
-	function __construct($path){	
-		$str = file_get_contents($path);
-		$l = simplexml_load_string($str);
+  function __construct( $path ) { 
+    $fileContents = file_get_contents( $path );
+    $xml = simplexml_load_string( $fileContents );
 
-		foreach ($l->dict->dict->dict as $track){
-	
-			preg_match_all("/<key>(.*)<\/key><.*>(.*)<\/.*>/", $track->asXML(), $m);
-			$t = new track();
-			foreach ($m[1] as $k => $v){
-				$t->{str_replace(" ", "_", $v)} = strval($m[2][$k]);
-			}
+    foreach ( $xml->dict->dict->dict as $trackObject ) {
+      preg_match_all( "/\<key\>(.+)\<\/key\>\<.+\>(.+)\<\/.+\>/", $trackObject->asXML(), $matches );
+      $track = new track();
+            
+      foreach ( $matches[1] as $key => $value ) {
+        $track->{ str_replace( " ", "_", $value ) } = str_replace( "&amp;", "&", str_replace( "\"", "”", strval( $matches[2][$key] ) ) );
+      }
 
-			$this->addTrack($t);
-		}
+      preg_match_all( "/\<key\>(.+)\<\/key\>\<(true|false)\/\>/", $trackObject->asXML(), $matches );
+      foreach ( $matches[1] as $key => $value ) {
+        $track->{ str_replace( " ", "_", $value ) } = $matches[2][$key];
+      }
 
-	}
+      $this->addTrack( $track );
+    }
+  }
 
-	function getTracks(){ return $this->_tracks; } 
-	function addTrack($__t){ $this->_tracks[] = $__t;} 
-	function removeTrack($__k){ unset($this->_tracks[$__k]); }
+  public function getTracks() { return $this->_tracks; } 
+  private function addTrack( $track ) { $this->_tracks[] = $track; } 
+  private function removeTrack( $__k ) { unset( $this->_tracks[$__k] ); }
+  public function getCount() { return count( $this->_tracks ); }
+  
+  public function getStarRating( $rating ) {
+    switch ( $rating ) {
+      case 100:
+        return "*****";
+      case 80:
+        return "****";
+      case 60:
+        return "***";
+      case 40:
+        return "**";
+      case 20:
+        return "*";
+      default:
+        return "";
+    }
+  }
+
 }
